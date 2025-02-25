fbfe2b7ae3fb75ee99b0404c3dd13cfe43e9c026
zeeshan <zeeshanb34@gmail.com>
2025-02-24 22:46:22 -0500

    final time

--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,19 @@
-// page.tsx
 "use client";
 import Hero from "./components/hero-section/Hero";
 import Works from "./components/work-section/Works";
 import About from "./components/about-section/About";
 import Contact from "./components/contact+footer/Contact";
 import Footer from "./components/contact+footer/Footer";
-import AccCompPage from "./pages/AccCompPage";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { initialBlobityOptions } from "@/utils/blobity.config";
 import useBlobity from "blobity/lib/react/useBlobity";
+import AccCompPage from "./pages/AccCompPage";
 
 export default function Home() {
   const blobity = useBlobity(initialBlobityOptions);
 
   return (
-    <Router>
-      <main className="overflow-x-hidden sm:overflow-x-visible">
-        <Routes>
-          <Route path="/" element={
-            <>
-              <Hero />
-              <Works />
-              <About />
-              <Contact />
-              <Footer />
-            </>
-          } />
-          <Route path="/acc-comp" element={<AccCompPage />} />
-        </Routes>
-      </main>
-    </Router>
+    <main className="overflow-x-hidden sm:overflow-x-visible">
+      <AccCompPage />
+    </main>
   );
 }
--- app/pages/AccCompPage.tsx
+++ app/pages/AccCompPage.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import './AccCompPage.css';
+// @ts-ignore
 
 const AccCompPage = () => {
 
